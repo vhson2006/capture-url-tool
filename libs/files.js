c0d233c99338266fad04eb7d2fd548f3ccff854d
Son Van <vhson2006@gmail.com>
2022-11-21 19:54:20 +0700

    create outputs folder if doesnt exist

--- libs/files.js
+++ libs/files.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const path = require('path');
 
 const emptyFolder = (folder) => {
+  if (!fs.existsSync(folder)){
+    fs.mkdirSync(folder);
+  }
   fs.readdir(folder, (err, files) => {
     if (err) throw err;
 
